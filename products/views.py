01d951161bf8890b69484bf2fdbc34754322ce60
Joseph Smith <jts272@outlook.com>
2023-07-31 18:24:44 +0100

    feat: add view with all products context

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Product
+
+
 # Create your views here.
+def all_products(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "products/index.html", context)
