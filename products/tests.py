699a2d5d5c6a07ad358cd969922819641b715a20
Joseph Smith <jts272@outlook.com>
2023-07-31 15:25:34 +0100

    test: category and product model str methods pass

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
+from .models import Category, Product
+
+
 # Create your tests here.
+class TestCategoryModel(TestCase):
+    def setUp(self):
+        self.c = Category.objects.create(name="test category", slug="test-category")
+
+    def test_category_str_method(self):
+        self.assertEqual(self.c.__str__(), "test category")
+
+
+class TestProductModel(TestCase):
+    def setUp(self):
+        self.p = Product.objects.create(name="test product", price=10.00)
+
+    def test_product_str_method(self):
+        self.assertEqual(self.p.__str__(), "test product")
