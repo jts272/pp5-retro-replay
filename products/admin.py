aa5771a220968262a3402132232c8ba29790b517
Joseph Smith <jts272@outlook.com>
2023-07-31 14:16:31 +0100

    feat: register products app models to Django admin
    A superuser was created using the Django CLI to access the admin site.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Category, Product
+
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ["name"]}
+
+
 # Register your models here.
+admin.site.register(Category, CategoryModelAdmin)
+admin.site.register(Product)
