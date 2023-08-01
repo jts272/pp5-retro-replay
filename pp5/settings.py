a3b5036b2b7bf45b3b4a33a4f018dc5ac2426db0
Joseph Smith <jts272@outlook.com>
2023-08-01 14:33:40 +0100

    feat: register products context processor

--- pp5/settings.py
+++ pp5/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 "django.contrib.messages.context_processors.messages",
                 # Custom context processors
                 "products.context_processors.categories",
+                "products.context_processors.platforms",
             ],
         },
     },
