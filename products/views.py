966054e813a069ce0306bc979f3c91729f3f28d8
Joseph Smith <jts272@outlook.com>
2023-08-01 16:32:23 +0100

    docs: update view docstrings in products app

--- products/views.py
+++ products/views.py
@@ -5,17 +5,28 @@ from .models import Product
 
 # Create your views here.
 def all_products(request):
+    """Returns all products from the database.
+
+    Arguments:
+        request -- HttpRequest
+
+    Returns:
+        HTML template with request and context variables available.
+    """
     products = Product.objects.all()
     context = {"products": products}
     return render(request, "products/index.html", context)
 
 
 def products_by_category(request, category):
-    """Returns all products within a given category
+    """Returns all products within a given category.
 
     Arguments:
         request -- HttpRequest
         category -- category to filter products by
+
+    Returns:
+        HTML template with request and context variables available.
     """
     products = Product.objects.filter(category__slug=category)
     context = {"products": products}
@@ -23,11 +34,14 @@ def products_by_category(request, category):
 
 
 def products_by_platform(request, platform):
-    """Returns all products within a given platform
+    """Returns all products within a given platform.
 
     Arguments:
         request -- HttpRequest
         platform -- platform to filter products by
+
+    Returns:
+        HTML template with request and context variables available.
     """
     products = Product.objects.filter(platform__slug=platform)
     context = {"products": products}
@@ -42,7 +56,7 @@ def products_by_region(request, region):
         region -- region to filter products by
 
     Returns:
-        HTML template with request and context variables available
+        HTML template with request and context variables available.
     """
     products = Product.objects.filter(region__slug=region)
     context = {"products": products}
