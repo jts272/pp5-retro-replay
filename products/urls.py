69175542b6249bda00232d5e7c807a521e8d67d9
Joseph Smith <jts272@outlook.com>
2023-08-01 13:24:49 +0100

    feat: add url to view products by category

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,11 @@ from django.urls import path
 from . import views
 
 app_name = "products"
-urlpatterns = [path("", views.all_products, name="all_products")]
+urlpatterns = [
+    path("", views.all_products, name="all_products"),
+    path(
+        "categories/<slug:category>/",
+        views.products_by_category,
+        name="products_by_category",
+    ),
+]
