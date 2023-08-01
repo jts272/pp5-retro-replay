02b2e0b8a0e4f217c61073e85ebab87965953fb5
Joseph Smith <jts272@outlook.com>
2023-08-01 13:41:59 +0100

    feat: add platform model and FK to product

--- products/models.py
+++ products/models.py
@@ -8,6 +8,9 @@ class Product(models.Model):
     category = models.ForeignKey(
         "Category", on_delete=models.SET_NULL, null=True, blank=True
     )
+    platform = models.ForeignKey(
+        "Platform", on_delete=models.SET_NULL, null=True, blank=True
+    )
 
     def __str__(self):
         return self.name
@@ -19,6 +22,14 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
 
     def __str__(self):
         return self.name
