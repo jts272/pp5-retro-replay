56209478e4b035ae7fae8f38cbbfe697da218d93
Joseph Smith <jts272@outlook.com>
2023-08-02 18:49:16 +0100

    test: home view pass

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class TestHomeView(TestCase):
+    def test_view_url_200_response(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
