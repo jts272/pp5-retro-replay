2035ee3aa62a2936913a8d6872052f4d0ec481c0
Joseph Smith <jts272@outlook.com>
2023-08-02 18:24:49 +0100

    docs: add docstring for home view

--- home/views.py
+++ home/views.py
@@ -5,6 +5,17 @@ from products.models import Product
 
 # Create your views here.
 def home(request):
+    """A view to render the home page and a selection of recent products.
+
+    List slice notation is used to determine the number of recent products
+    to return.
+
+    Arguments:
+        request -- HttpRequest
+
+    Returns:
+        HTML template with request and context variables available
+    """
     recent_products = Product.objects.all()[:3]
     context = {"recent_products": recent_products}
     return render(request, "home/home.html", context)
