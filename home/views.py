5102489b04effca73d73ffd0cafdfcf18d162b16
Joseph Smith <jts272@outlook.com>
2023-08-02 18:02:02 +0100

    feat: add home view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from products.models import Product
+
+
 # Create your views here.
+def home(request):
+    recent_products = Product.objects.all()[:3]
+    context = {"recent_products": recent_products}
+    return render(request, "home/home.html", context)
