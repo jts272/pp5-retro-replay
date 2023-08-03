e4e013d07ba97ba0d79d6f6b78701e30254e4308
Joseph Smith <jts272@outlook.com>
2023-08-03 20:33:27 +0100

    feat: add initial basket ajax script

--- static/js/basket.js
+++ static/js/basket.js
@@ -1,1 +1,18 @@
-console.log("Hello from static/js/basket.js !");
+// Reference: https://youtu.be/VOwfGW-ZTIY?list=PLOLrQ9Pn6caxY4Q1U9RjO1bulQp5NDYS_&t=7011
+
+$(document).on("click", "#add-to-basket", function (e) {
+  e.preventDefault();
+  $.ajax({
+    type: "POST",
+    // Connect to a URL, which is connected to a view
+    url: null, // Must be added in inline script to utilize Django template tags
+    data: {
+      productId: $("add-to-basket").val(),
+      csrfmiddlewaretoken: null, // Get inline from template
+      action: "post",
+    },
+    // Data that is returned
+    success: function (json) {},
+    error: function (xhr, errmsg, err) {},
+  });
+});
