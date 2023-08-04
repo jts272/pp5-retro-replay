07208d2d9e725a63e2b71e495ceed6011af44c83
Joseph Smith <jts272@outlook.com>
2023-08-04 13:28:36 +0100

    feat: add url path for add to basket view

--- basket/urls.py
+++ basket/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = "basket"
-urlpatterns = [path("", views.basket_summary, name="basket_summary")]
+urlpatterns = [
+    path("", views.basket_summary, name="basket_summary"),
+    path("add/", views.add_to_basket, name="add_to_basket"),
+]
