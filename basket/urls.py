c13c29718f88fef7000314f4d3254acd9662dc01
Joseph Smith <jts272@outlook.com>
2023-08-05 13:14:25 +0100

    feat: add url path for remove from basket view

--- basket/urls.py
+++ basket/urls.py
@@ -6,4 +6,5 @@ app_name = "basket"
 urlpatterns = [
     path("", views.basket_summary, name="basket_summary"),
     path("add/", views.add_to_basket, name="add_to_basket"),
+    path("remove/", views.remove_from_basket, name="remove_from_basket"),
 ]
