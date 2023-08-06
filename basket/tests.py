258017705d38658e7cce5516c9bcfa5d13b0a2bc
Joseph Smith <jts272@outlook.com>
2023-08-06 12:02:38 +0100

    test: basket is iterable test pass
    This test is passing, however the coverage report still shows missing tests for the Basket class' '__iter__()' method.

--- basket/tests.py
+++ basket/tests.py
@@ -1,5 +1,7 @@
+from django.http import HttpRequest
 from django.test import TestCase
 
+from basket.basket import Basket
 from products.models import Product
 
 
@@ -29,14 +31,43 @@ class TestAddToBasketView(TestCase):
 class TestRemoveFromBasketView(TestCase):
     def setUp(self):
         self.p = Product.objects.create(name="test product", price=10.00)
+        self.p = Product.objects.create(name="test product 2", price=20.00)
         self.client.post(
             "/basket/add/",
             {"productId": 1, "action": "post"},
+        )
+        self.client.post(
+            "/basket/add/",
+            {"productId": 2, "action": "post"},
         )
 
     def test_view_url_json_response(self):
         response = self.client.post(
             "/basket/remove/",
-            {"productId": 1, "action": "post"},
+            {"productId": 2, "action": "post"},
         )
-        self.assertTrue(response.json(), {"basket quantity": 0})
+        self.assertTrue(
+            response.json(), {"basket quantity": 1, "product removed": 2}
+        )
+
+
+class TestBasketClass(TestCase):
+    def setUp(self):
+        # Generate a request object
+        request = HttpRequest()
+        # Generate a session object
+        session = self.client.session
+        # Assign the session to the request
+        request.session = session
+        # Create an instance of the Basket class
+        self.b = Basket(request)
+
+    def test_basket_class_is_iterable(self):
+        """Current adding and removing tests to not meet coverage for
+        the basket class' '__iter__' method. This test is designed to
+        test for the presence of such a method.
+
+        As the basket is tied to Django sessions, the set up instantiates
+        a request object that has session data.
+        """
+        self.assertTrue(hasattr(self.b, "__iter__"))
