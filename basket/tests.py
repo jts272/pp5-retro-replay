655930a0da0917b607f8410db933244b9f74526c
Joseph Smith <jts272@outlook.com>
2023-08-06 11:15:30 +0100

    test: basket views pass

--- basket/tests.py
+++ basket/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
 
+from products.models import Product
+
+
 # Create your tests here.
+class TestBasketView(TestCase):
+    def test_view_url_200_response(self):
+        response = self.client.get("/basket/")
+        self.assertEqual(response.status_code, 200)
+
+
+class TestAddToBasketView(TestCase):
+    def setUp(self):
+        self.p = Product.objects.create(name="test product", price=10.00)
+
+    def test_view_url_json_response(self):
+        """Test for the json response from the ajax function to add to basket.
+
+        Reference: https://youtu.be/VOwfGW-ZTIY?list=PLOLrQ9Pn6caxY4Q1U9RjO1bulQp5NDYS_&t=15091
+        """
+        response = self.client.post(
+            "/basket/add/",
+            {"productId": 1, "action": "post"},
+        )
+        self.assertTrue(response.json(), {"product just added": 1})
+
+
+class TestRemoveFromBasketView(TestCase):
+    def setUp(self):
+        self.p = Product.objects.create(name="test product", price=10.00)
+        self.client.post(
+            "/basket/add/",
+            {"productId": 1, "action": "post"},
+        )
+
+    def test_view_url_json_response(self):
+        response = self.client.post(
+            "/basket/remove/",
+            {"productId": 1, "action": "post"},
+        )
+        self.assertTrue(response.json(), {"basket quantity": 0})
