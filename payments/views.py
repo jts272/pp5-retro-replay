33d42771a4261b2638231b3ec24293f59067853c
Joseph Smith <jts272@outlook.com>
2023-08-08 15:34:42 +0100

    feat: add basic view to display basket contents at checkout

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from basket.basket import Basket
+
+
 # Create your views here.
+def checkout(request):
+    basket = Basket(request)
+    context = {"basket": basket}
+    return render(request, "basket/basket.html", context)
