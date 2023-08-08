ac066b616d309d9e5500c2dab9c8f0170798334a
Joseph Smith <jts272@outlook.com>
2023-08-08 16:48:37 +0100

    feat: get basket subtotal in int format in checkout view

--- payments/views.py
+++ payments/views.py
@@ -5,6 +5,14 @@ from basket.basket import Basket
 
 # Create your views here.
 def checkout(request):
+    # https://youtu.be/ncsCnC3Ynlw?list=PLOLrQ9Pn6caxY4Q1U9RjO1bulQp5NDYS_&t=12452
     basket = Basket(request)
+
+    # Get subtotal as integer with no decimals for Stripe
+    total = str(basket.get_subtotal())
+    total = total.replace(".", "")
+    total = int(total)
+    print(total)
+
     context = {"basket": basket}
     return render(request, "payments/payments.html", context)
