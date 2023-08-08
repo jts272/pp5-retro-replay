092a2482bc803f3d53a47747456b7918acbe6aa8
Joseph Smith <jts272@outlook.com>
2023-08-08 17:03:50 +0100

    feat: create payment intent and return client secret in checkout

--- payments/views.py
+++ payments/views.py
@@ -1,6 +1,12 @@
+import os
+
+import stripe
 from django.shortcuts import render
 
 from basket.basket import Basket
+
+if os.path.isfile("env.py"):
+    import env  # noqa
 
 
 # Create your views here.
@@ -14,5 +20,13 @@ def checkout(request):
     total = int(total)
     print(total)
 
-    context = {"basket": basket}
+    stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+
+    intent = stripe.PaymentIntent.create(
+        amount=total, currency="gbp", metadata={"user_id": request.user.pk}
+    )
+
+    print(intent.client_secret)
+
+    context = {"basket": basket, "client_secret": intent.client_secret}
     return render(request, "payments/payments.html", context)
