2c35d8820270e5a71080c7577a56080fbca23302
Joseph Smith <jts272@outlook.com>
2023-08-10 20:33:39 +0100

    refactor: add comments and remove console logs in checkout script

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,23 +1,14 @@
+// Set up Stripe with publishable key
 const stripe = Stripe(
   "pk_test_51NMu8PEPb8OObKzZJEtyxn1AEAmJVbitHxGiYBMIOoVDEHgedK0qnuQexEHWPB3kbmS5C66CWj9uNtQCQRdTq9Px00oxNbOOdG"
 );
 
+// Get client secret from the payment intent created in the checkout view
 const payBtn = document.getElementById("pay-btn");
 const clientSecret = payBtn.getAttribute("data-stripe-client-secret");
-console.log(clientSecret);
 
+// Set up Stripe.js Elements, using the client secret
 const elements = stripe.elements({ clientSecret: clientSecret });
-
-// const options = {
-//   // Fully customizable with appearance API.
-//   appearance: {
-//     /* ... */
-//   },
-// };
-
-// // Only need to create this if no elements group exist yet.
-// // Create a new Elements instance if needed, passing the
-// // optional appearance object.
 
 // Create and mount the Address Element in shipping mode
 const addressElement = elements.create("address", {
@@ -26,18 +17,6 @@ const addressElement = elements.create("address", {
 });
 addressElement.mount("#address-element");
 
-addressElement.on("change", (event) => {
-  if (event.complete) {
-    // Extract potentially complete address
-    const address = event.value.address;
-    console.log(address);
-    console.log(event);
-  }
-});
-
+// Create and mount the Payment Element
 const paymentElement = elements.create("payment");
 paymentElement.mount("#payment-element");
-
-paymentElement.on("change", (event) => {
-  console.log(event);
-});
