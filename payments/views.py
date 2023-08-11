faca1e6d0c62bc8529ed5f0c266ae7a40fed6267
Joseph Smith <jts272@outlook.com>
2023-08-11 13:47:54 +0100

    fix: #61 build payment intent only when basket is truthy
    Requests to make Stripe payment intent with a null 'amount' value in the view would crash the page. The payment intent is now only created when the basket has a value. Furthermore, the script that instantiates the Stripe elements is only included when the basket is truthy within the template context.

--- payments/views.py
+++ payments/views.py
@@ -17,21 +17,31 @@ def checkout(request):
     # https://youtu.be/ncsCnC3Ynlw?list=PLOLrQ9Pn6caxY4Q1U9RjO1bulQp5NDYS_&t=12452
     basket = Basket(request)
 
-    # Get subtotal as integer with no decimals for Stripe
-    total = str(basket.get_subtotal())
-    total = total.replace(".", "")
-    total = int(total)
-    print(total)
+    if basket:
+        # Get subtotal as integer with no decimals for Stripe
+        total = str(basket.get_subtotal())
+        total = total.replace(".", "")
+        total = int(total)
+        print(total)
 
-    stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+        stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
-    intent = stripe.PaymentIntent.create(
-        amount=total, currency="gbp", metadata={"user_id": request.user.pk}
-    )
+        intent = stripe.PaymentIntent.create(
+            amount=total, currency="gbp", metadata={"user_id": request.user.pk}
+        )
 
-    print(intent.client_secret)
+        print(intent.client_secret)
 
-    context = {"basket": basket, "client_secret": intent.client_secret}
+        context = {
+            "basket": basket,
+            # "client_secret": intent.client_secret
+        }
+    else:
+        context = {
+            "basket": basket,
+            # "client_secret": intent.client_secret
+        }
+        print(basket.basket)
     return render(request, "payments/payments.html", context)
 
 
