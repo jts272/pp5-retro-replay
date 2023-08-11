e4c6a846643aac7b00928350859d66fb8ac4ece5
Joseph Smith <jts272@outlook.com>
2023-08-11 13:05:55 +0100

    feat: add example endpoint for Stripe webhooks

--- payments/views.py
+++ payments/views.py
@@ -1,8 +1,11 @@
+import json
 import os
 
 import stripe
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
 from basket.basket import Basket
 
 if os.path.isfile("env.py"):
@@ -30,3 +33,36 @@ def checkout(request):
 
     context = {"basket": basket, "client_secret": intent.client_secret}
     return render(request, "payments/payments.html", context)
+
+
+@require_POST
+@csrf_exempt
+def webhook_view(request):
+    # Reference: https://stripe.com/docs/webhooks#example-endpoint
+    payload = request.body
+    event = None
+
+    try:
+        event = stripe.Event.construct_from(
+            json.loads(payload), stripe.api_key
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+
+    # Handle the event
+    if event.type == "payment_intent.succeeded":
+        payment_intent = event.data.object  # contains a stripe.PaymentIntent
+        # Then define and call a method to handle the successful payment intent.
+        # handle_payment_intent_succeeded(payment_intent)
+        print(f"webhook payment intent: {payment_intent}")
+    elif event.type == "payment_method.attached":
+        payment_method = event.data.object  # contains a stripe.PaymentMethod
+        # Then define and call a method to handle the successful attachment of a PaymentMethod.
+        # handle_payment_method_attached(payment_method)
+        print(f"webhook payment_method: {payment_method}")
+    # ... handle other event types
+    else:
+        print("Unhandled event type {}".format(event.type))
+
+    return HttpResponse(status=200)
