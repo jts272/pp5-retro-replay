c137c655759135295afbb951a06b9a2dcbc0dea5
Joseph Smith <jts272@outlook.com>
2023-08-11 13:07:53 +0100

    feat: add route for webhook view

--- payments/urls.py
+++ payments/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = "payments"
-urlpatterns = [path("checkout/", views.checkout, name="checkout")]
+urlpatterns = [
+    path("checkout/", views.checkout, name="checkout"),
+    path("webhook/", views.webhook_view, name="webhook"),
+]
