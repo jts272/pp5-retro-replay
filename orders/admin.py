1ccc9bfb40a834a5afcc6719c7bd876fa0a32c40
Joseph Smith <jts272@outlook.com>
2023-08-11 19:16:10 +0100

    feat: create and register order model admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Order
+
+
+class OrderModelAdmin(admin.ModelAdmin):
+    list_display = [
+        "order_id",
+        "name",
+        "amount",
+        "created",
+        "updated",
+        "paid",
+    ]
+
+
 # Register your models here.
+admin.site.register(Order, OrderModelAdmin)
