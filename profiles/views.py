ccc3ca3fb2e59ac209e593980076ad3c83285f38
Joseph Smith <jts272@outlook.com>
2023-08-15 13:05:04 +0100

    feat: add login requirement to access profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 # Create your views here.
+@login_required
 def profile(request):
     """Display the user's profile to perform actions relating to saved
     addresses and past orders.
