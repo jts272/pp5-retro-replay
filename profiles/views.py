82e8a78c03f204529681ae84a7a83d192361f145
Joseph Smith <jts272@outlook.com>
2023-08-15 15:43:57 +0100

    feat: render user's profile context in profile template
    Existing users were assigned profiles in the shell. A new user was registered and verified through the front end process to confirm that the profile creation signal was working correctly.

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from profiles.models import Profile
 
 
 # Create your views here.
@@ -11,5 +13,6 @@ def profile(request):
     Arguments:
         request -- HttpRequest
     """
-    context = {}
+    profile = get_object_or_404(Profile, user=request.user)
+    context = {"profile": profile}
     return render(request, "profiles/profile.html", context)
