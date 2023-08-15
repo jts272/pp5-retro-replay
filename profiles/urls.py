3492735586774fce8c5aa03f38673ae39aed0bbc
Joseph Smith <jts272@outlook.com>
2023-08-15 16:32:10 +0100

    feat: setup list and detail views for orders and addresses.
    The profile page will serve as a landing zone where the customer can choose to view their orders or manage their addresses.

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,13 @@ from django.urls import path
 from . import views
 
 app_name = "profiles"
-urlpatterns = [path("", views.profile, name="profile")]
+urlpatterns = [
+    path("", views.profile, name="profile"),
+    path("orders/", views.order_list, name="order_list"),
+    path("orders/<slug:order_id>/", views.order_detail, name="order_detail"),
+    path("addresses/", views.address_list, name="address_list"),
+    path(
+        "addresses/<slug:uuid>/", views.address_detail, name="address_detail"
+    ),
+    # Include urls for editing views as they are made
+]
