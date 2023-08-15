3ca2879ec7367e66cffd306cb12ef936c4b3e74b
Joseph Smith <jts272@outlook.com>
2023-08-15 12:47:17 +0100

    feat: add basic profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def profile(request):
+    """Display the user's profile to perform actions relating to saved
+    addresses and past orders.
+
+    Arguments:
+        request -- HttpRequest
+    """
+    context = {}
+    return render(request, "profiles/profile.html", context)
