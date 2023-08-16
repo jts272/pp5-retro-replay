c34fcaa18080f71488088964974d92709d69229d
Joseph Smith <jts272@outlook.com>
2023-08-16 15:56:40 +0100

    feat: add basic conditional address list template
    If no addresses are found on the profile, the user is informed that they may add one.

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", views.profile, name="profile"),
     path("orders/", views.order_list, name="order_list"),
     path("orders/<slug:order_id>/", views.order_detail, name="order_detail"),
-    # path("addresses/", views.address_list, name="address_list"),
+    path("addresses/", views.address_list, name="address_list"),
     # path(
     #     "addresses/<slug:uuid>/", views.address_detail, name="address_detail"
     # ),
