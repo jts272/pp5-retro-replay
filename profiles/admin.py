c5567b46f35bf05788d8f41c40391f39b1c91f0d
Joseph Smith <jts272@outlook.com>
2023-08-16 19:43:46 +0100

    feat: add uuid read-only field in profile address admin
    The uuid can easily be copied from the admin panel to test the url slug required for the edit view.

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Address, Profile
 
+
+class AddressModelAdmin(admin.ModelAdmin):
+    readonly_fields = ["uuid"]
+
+
 # Register your models here.
-admin.site.register(Address)
+admin.site.register(Address, AddressModelAdmin)
 admin.site.register(Profile)
