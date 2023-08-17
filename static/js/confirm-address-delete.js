6e855bf3fb6932d175d89dcef14059a827ab290a
Joseph Smith <jts272@outlook.com>
2023-08-17 13:51:04 +0100

    feat: add option to close address deletion action button
    Fixes #67 where the focusout event was preventing the actual object deletion from happening. Now the user can close the controls manually.

--- static/js/confirm-address-delete.js
+++ static/js/confirm-address-delete.js
@@ -5,17 +5,22 @@ const confirmDeleteBtns = Array.from(
 const actualDeleteBtns = Array.from(
   document.getElementsByClassName("delete-do")
 );
+const goBackBtns = Array.from(
+  document.getElementsByClassName("delete-go-back")
+);
 
 // Iterate through each 'ask' button on the page
 for (let i = 0; i < confirmDeleteBtns.length; i++) {
   const confirmBtn = confirmDeleteBtns[i];
   const deleteBtn = actualDeleteBtns[i];
+  const backBtn = goBackBtns[i];
 
   // Open the clicked 'ask' button to reveal the corresponding 'do' delete button
   confirmBtn.addEventListener("click", function (event) {
     deleteBtn.classList.toggle("visually-hidden");
   });
-  confirmBtn.addEventListener("focusout", function (event) {
-    deleteBtn.classList.add("visually-hidden");
+  // Provide user option to go back and close the 'do' delete controls
+  backBtn.addEventListener("click", function (event) {
+    deleteBtn.classList.toggle("visually-hidden");
   });
 }
