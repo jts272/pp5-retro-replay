b20c6577ab2c87da970cd74d256a73cd6b4b1c6b
Joseph Smith <jts272@outlook.com>
2023-08-21 13:48:15 +0100

    feat: register FAQ admin with summernote field

--- support/admin.py
+++ support/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import FAQ
+
+
+class FAQAdmin(SummernoteModelAdmin):
+    fields = ["question", "answer", "created", "updated", "published"]
+    summernote_fields = ["answer"]
+    readonly_fields = ["created", "updated"]
+
 
 # Register your models here.
+admin.site.register(FAQ, FAQAdmin)
