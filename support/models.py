5571ac570eee160353b9ed7694af0ea0c2f8c9c9
Joseph Smith <jts272@outlook.com>
2023-08-21 13:38:32 +0100

    feat: create and migrate basic FAQ model

--- support/models.py
+++ support/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class FAQ(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=True)
