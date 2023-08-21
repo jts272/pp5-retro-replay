d903fde4a195a5ce3522f46ccde40615a9ed1a3a
Joseph Smith <jts272@outlook.com>
2023-08-21 14:01:07 +0100

    feat: provide published FAQs to the support view context

--- support/views.py
+++ support/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import FAQ
 
 
 # Create your views here.
 def support(request):
-    return render(request, "support/support.html")
+    faqs = FAQ.objects.filter(published=True)
+    context = {"faqs": faqs}
+    return render(request, "support/support.html", context)
