aa05de17a0d63a328dc85856788eb63b45028323
Joseph Smith <jts272@outlook.com>
2023-08-21 16:45:08 +0100

    feat: add proposed urls for FAQ generic editing views

--- support/urls.py
+++ support/urls.py
@@ -3,4 +3,9 @@ from django.urls import path
 from . import views
 
 app_name = "support"
-urlpatterns = [path("", views.support, name="support")]
+urlpatterns = [
+    path("", views.support, name="support"),
+    # path("faq/add/", views.AddFAQ.as_view(), name="add_faq"),
+    # path("faq/<int:pk>/update/", views.UpdateFAQ.as_view(), name="update_faq"),
+    # path("faq/<int:pk>/delete/", views.DeleteFAQ.as_view(), name="delete_faq"),
+]
