616687551706305a1608d965124bacf841761f85
Joseph Smith <jts272@outlook.com>
2023-08-21 22:36:46 +0100

    feat: create model for collecting customer queries

--- support/models.py
+++ support/models.py
@@ -17,3 +17,29 @@ class FAQ(models.Model):
 
     def __str__(self):
         return self.question
+
+
+class CustomerQuery(models.Model):
+    sender = models.EmailField(
+        help_text="Reply to your customer at this address."
+    )
+    query = models.CharField(
+        max_length=400,
+        help_text=(
+            "Please outline your query for us in less than 400 characters."
+        ),
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    replied = models.BooleanField(
+        default=False,
+        help_text=(
+            "Keep track of which customer queries you have replied to.",
+        ),
+    )
+
+    class Meta:
+        verbose_name_plural = "Customer queries"
+
+    def __str__(self):
+        return f"Query on {self.created} from {self.sender}"
