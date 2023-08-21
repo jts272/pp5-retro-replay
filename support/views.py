85f863688449c9b17db75b7b8a171629842643d5
Joseph Smith <jts272@outlook.com>
2023-08-21 12:55:42 +0100

    feat: add support page template extending from base template

--- support/views.py
+++ support/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def support(request):
-    return HttpResponse("This is the support page")
+    return render(request, "support/support.html")
