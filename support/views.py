5aa32371cc1b690b6779d4f727f4c3add90d919e
Joseph Smith <jts272@outlook.com>
2023-08-21 22:03:03 +0100

    feat: add permission tests to FAQ CRUD views

--- support/views.py
+++ support/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -15,24 +16,40 @@ def support(request):
     return render(request, "support/support.html", context)
 
 
-class AddFAQ(SuccessMessageMixin, CreateView):
+class AddFAQ(
+    LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, CreateView
+):
     model = FAQ
     form_class = FAQForm
     success_url = reverse_lazy("support:support")
     success_message = "New FAQ has been added!"
 
+    def test_func(self):
+        user = self.request.user
+        return user.is_superuser
 
-class UpdateFAQ(SuccessMessageMixin, UpdateView):
+
+class UpdateFAQ(
+    LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView
+):
     model = FAQ
     form_class = FAQForm
     success_url = reverse_lazy("support:support")
     success_message = "Your FAQ has been updated."
 
+    def test_func(self):
+        user = self.request.user
+        return user.is_superuser
 
-class DeleteFAQ(DeleteView):
+
+class DeleteFAQ(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = FAQ
     success_url = reverse_lazy("support:support")
     success_message = "Your FAQ has been deleted."
+
+    def test_func(self):
+        user = self.request.user
+        return user.is_superuser
 
     # Give message for deletion - cannot use mixin to hook to form_valid
     # Reference: https://stackoverflow.com/questions/24822509/success-message-in-deleteview-not-shown
