fd37e46bc3e3511662b578b93dff53967231528c
Joseph Smith <jts272@outlook.com>
2023-08-21 17:30:28 +0100

    feat: add success messages to FAQ CRUD functions

--- support/views.py
+++ support/views.py
@@ -1,9 +1,10 @@
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-
-from django_summernote.widgets import SummernoteInplaceWidget
 from django_summernote.fields import SummernoteTextField
+from django_summernote.widgets import SummernoteInplaceWidget
 
 from .models import FAQ
 
@@ -15,22 +16,31 @@ def support(request):
     return render(request, "support/support.html", context)
 
 
-class AddFAQ(CreateView):
+class AddFAQ(SuccessMessageMixin, CreateView):
     model = FAQ
     fields = ["question", "answer", "published"]
     answer = SummernoteTextField()
     widgets = {"answer": SummernoteInplaceWidget()}
     success_url = reverse_lazy("support:support")
+    success_message = "New FAQ has been added!"
 
 
-class UpdateFAQ(UpdateView):
+class UpdateFAQ(SuccessMessageMixin, UpdateView):
     model = FAQ
     fields = ["question", "answer", "published"]
     answer = SummernoteTextField()
     widgets = {"answer": SummernoteInplaceWidget()}
     success_url = reverse_lazy("support:support")
+    success_message = "Your FAQ has been updated."
 
 
 class DeleteFAQ(DeleteView):
     model = FAQ
     success_url = reverse_lazy("support:support")
+    success_message = "Your FAQ has been deleted."
+
+    # Give message for deletion - cannot use mixin to hook to form_valid
+    # Reference: https://stackoverflow.com/questions/24822509/success-message-in-deleteview-not-shown
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(DeleteFAQ, self).delete(request, *args, **kwargs)
