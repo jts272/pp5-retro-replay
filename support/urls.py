65265d23acc1429d0954336f78298734fc8f79a6
Joseph Smith <jts272@outlook.com>
2023-08-21 16:50:47 +0100

    feat: add generic editing views for FAQs and enable urls

--- support/urls.py
+++ support/urls.py
@@ -5,7 +5,11 @@ from . import views
 app_name = "support"
 urlpatterns = [
     path("", views.support, name="support"),
-    # path("faq/add/", views.AddFAQ.as_view(), name="add_faq"),
-    # path("faq/<int:pk>/update/", views.UpdateFAQ.as_view(), name="update_faq"),
-    # path("faq/<int:pk>/delete/", views.DeleteFAQ.as_view(), name="delete_faq"),
+    path("faqs/add/", views.AddFAQ.as_view(), name="add_faq"),
+    path(
+        "faqs/<int:pk>/update/", views.UpdateFAQ.as_view(), name="update_faq"
+    ),
+    path(
+        "faqs/<int:pk>/delete/", views.DeleteFAQ.as_view(), name="delete_faq"
+    ),
 ]
--- support/views.py
+++ support/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
+from django_summernote.widgets import SummernoteInplaceWidget
+from django_summernote.fields import SummernoteTextField
+
 from .models import FAQ
 
 
@@ -7,3 +13,22 @@ def support(request):
     faqs = FAQ.objects.filter(published=True)
     context = {"faqs": faqs}
     return render(request, "support/support.html", context)
+
+
+class AddFAQ(CreateView):
+    model = FAQ
+    fields = ["question", "answer", "published"]
+    answer = SummernoteTextField()
+    widgets = {"answer": SummernoteInplaceWidget()}
+
+
+class UpdateFAQ(UpdateView):
+    model = FAQ
+    fields = ["question", "answer", "published"]
+    answer = SummernoteTextField()
+    widgets = {"answer": SummernoteInplaceWidget()}
+
+
+class DeleteFAQ(DeleteView):
+    model = FAQ
+    success_url = reverse_lazy("support:support")
