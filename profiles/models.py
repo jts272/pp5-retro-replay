40ec2cf7064cf2e4dfc4e2fb570d6719ed090d9d
Joseph Smith <jts272@outlook.com>
2023-08-22 14:06:38 +0100

    refactor: amend profile string method

--- profiles/models.py
+++ profiles/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.user.username}'s profile"
+        return self.user.username
 
 
 @receiver(post_save, sender=User)
