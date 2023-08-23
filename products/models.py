d7b7a641becccc18921042be7bd5f00372619531
Joseph Smith <jts272@outlook.com>
2023-08-23 16:15:18 +0100

    feat: add product image upload folder and style responsive product detail card

--- products/models.py
+++ products/models.py
@@ -31,7 +31,9 @@ class Product(models.Model):
     region = models.ForeignKey(
         "Region", on_delete=models.SET_NULL, null=True, blank=True
     )
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(
+        upload_to="product_images/", null=True, blank=True
+    )
     condition = models.CharField(
         max_length=20, choices=CONDITION_CHOICES, default="Very Good"
     )
