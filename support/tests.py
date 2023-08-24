c62a23ed84939ca6af153b94c4e6d0676fcd6edd
Joseph Smith <jts272@outlook.com>
2023-08-24 21:54:40 +0100

    feat: support view url and invalid form test pass

--- support/tests.py
+++ support/tests.py
@@ -1,7 +1,9 @@
-from django.test import TestCase
-from .models import FAQ, CustomerQuery
-
 import factory
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
+
+from .forms import CustomerQueryForm
+from .models import FAQ, CustomerQuery
 
 
 class FAQFactory(factory.django.DjangoModelFactory):
@@ -32,3 +34,25 @@ class TestCustomerQuery(TestCase):
         self.assertEqual(
             self.q.__str__(), "Query on 24/08/2023 from test@mail.com"
         )
+
+
+class TestSupportViews(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="tester", password="secret"
+        )
+        self.user.save()
+        self.c = Client()
+        self.c.login(username="tester", password="secret")
+
+    def test_support_view_url_200(self):
+        response = self.c.get("/support/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_not_form_is_valid(self):
+        form = CustomerQueryForm(
+            data={
+                "query": "@",
+            }
+        )
+        self.assertFalse(form.is_valid())
