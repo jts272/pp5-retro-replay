77ed71dbcaf1b4fe7610f1878e50fe79e26cd77f
Joseph Smith <jts272@outlook.com>
2023-08-24 21:01:15 +0100

    feat: include placeholder and error text for support query form

--- support/forms.py
+++ support/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from django_summernote.fields import SummernoteTextField
 from django_summernote.widgets import SummernoteWidget
@@ -18,4 +20,21 @@ class CustomerQueryForm(forms.ModelForm):
     class Meta:
         model = CustomerQuery
         fields = ["query"]
-        widgets = {"query": forms.widgets.Textarea()}
+        widgets = {
+            "query": forms.widgets.Textarea(
+                attrs={
+                    "placeholder": (
+                        "Enter you query here (minimum 20 characters)"
+                    )
+                }
+            )
+        }
+
+    def clean_query(self):
+        p = re.compile("[a-zA-z]{1-20}")
+        query = self.cleaned_data["query"]
+        if not p.match(query):
+            raise forms.ValidationError(
+                "Please provide some more detail before submitting your query."
+            )
+        return query
