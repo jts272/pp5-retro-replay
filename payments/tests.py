acf467a166572e256a0a73c4049f423cc966a0c0
Joseph Smith <jts272@outlook.com>
2023-08-24 22:02:45 +0100

    feat: checkout and checkout status url tests pass

--- payments/tests.py
+++ payments/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
+
 
 # Create your tests here.
+class TestBasketView(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="tester", password="secret"
+        )
+        self.user.save()
+        self.c = Client()
+        self.c.login(username="tester", password="secret")
+
+    def test_checkout_url_200(self):
+        response = self.c.get("/payments/checkout/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_checkout_status_url_200(self):
+        response = self.c.get("/payments/checkout-status/")
+        self.assertEqual(response.status_code, 200)
