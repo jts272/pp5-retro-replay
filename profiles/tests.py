850f9924399c0bd7da7e9678147f650caaa64777
Joseph Smith <jts272@outlook.com>
2023-08-24 19:10:23 +0100

    test: profile address form pass

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
+from .forms import ProfileAddressForm
+
+
 # Create your tests here.
+class TestProfileAddressForm(TestCase):
+    # https://www.obeythetestinggoat.com/book/chapter_simple_form.html
+    def test_not_form_is_valid(self):
+        form = ProfileAddressForm(
+            data={
+                "name": "@",
+                "address_line1": "@",
+                "address_line2": "@",
+                "city": "@",
+                "postal_code": "@",
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_form_is_valid(self):
+        form = ProfileAddressForm(
+            data={
+                "name": "Joe",
+                "address_line1": "123 Street",
+                "address_line2": "Flat A",
+                "city": "London",
+                "postal_code": "SW1A 2AA",
+            }
+        )
+        self.assertTrue(form.is_valid())
