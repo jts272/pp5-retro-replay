528a56beb9da0af9aee9ad1d31d7fb03ce34ee81
Joseph Smith <jts272@outlook.com>
2023-08-24 17:53:23 +0100

    test: order item model string method pass

--- orders/tests.py
+++ orders/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 import factory
-from .models import Order
+from .models import Order, OrderItem
+from products.models import Product, Platform, Region
 
 
 class OrderFactory(factory.Factory):
@@ -11,8 +12,49 @@ class OrderFactory(factory.Factory):
     created = "24/08/2023"
 
 
+class PlatformFactory(factory.Factory):
+    class Meta:
+        model = Platform
+
+    name = "test platform"
+
+
+class RegionFactory(factory.Factory):
+    class Meta:
+        model = Region
+
+    name = "pal"
+
+
+class ProductFactory(factory.Factory):
+    class Meta:
+        model = Product
+
+    name = "test product"
+    platform = PlatformFactory()
+    region = RegionFactory()
+
+
+class OrderItemFactory(factory.Factory):
+    class Meta:
+        model = OrderItem
+
+    order = OrderFactory()
+    product = ProductFactory()
+    id = 1
+
+
 # Create your tests here.
 class TestOrderModel(TestCase):
     def test_order_str_method(self):
         self.order = OrderFactory()
         self.assertEqual(self.order.__str__(), "Joe's order on 24/08/2023")
+
+
+class TestOrderItemModel(TestCase):
+    def test_order_item_str_method(self):
+        self.order_item = OrderItemFactory()
+        self.assertEqual(
+            self.order_item.__str__(),
+            "Order Item id: 1 - Item: test product for test platform - pal",
+        )
