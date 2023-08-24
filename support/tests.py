79fb898955dc4d431783ee87ebee321b9aef5474
Joseph Smith <jts272@outlook.com>
2023-08-24 20:42:05 +0100

    test: support models string methods pass

--- support/tests.py
+++ support/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .models import FAQ, CustomerQuery
+
+import factory
+
+
+class FAQFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = FAQ
+
+    question = "question"
+
+
+class CustomerQueryFactory(factory.Factory):
+    class Meta:
+        model = CustomerQuery
+
+    sender = "test@mail.com"
+    created = "24/08/2023"
+
 
 # Create your tests here.
+class TestFAQModel(TestCase):
+    def test_faq_str_method(self):
+        self.f = FAQFactory()
+        self.assertEqual(self.f.__str__(), "question")
+
+
+class TestCustomerQuery(TestCase):
+    def test_customer_query_str_method(self):
+        self.q = CustomerQueryFactory()
+        self.assertEqual(
+            self.q.__str__(), "Query on 24/08/2023 from test@mail.com"
+        )
