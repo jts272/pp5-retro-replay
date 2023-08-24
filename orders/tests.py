fa7d3ec694809dd1a353d0b48c79d12fca5317b7
Joseph Smith <jts272@outlook.com>
2023-08-24 17:35:07 +0100

    test: install factory boy for order string method pass

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+import factory
+from .models import Order
+
+
+class OrderFactory(factory.Factory):
+    class Meta:
+        model = Order
+
+    name = "Joe"
+    created = "24/08/2023"
+
 
 # Create your tests here.
+class TestOrderModel(TestCase):
+    def test_order_str_method(self):
+        self.order = OrderFactory()
+        self.assertEqual(self.order.__str__(), "Joe's order on 24/08/2023")
