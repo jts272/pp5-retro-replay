471e9ebd6e222ce5fcd31cd60875feb1c3daef12
Joseph Smith <jts272@outlook.com>
2023-08-25 17:05:22 +0100

    style: remove console log from message auto dismiss script

--- static/js/message-auto-dismiss.js
+++ static/js/message-auto-dismiss.js
@@ -1,6 +1,5 @@
 // Simulate clicking message close buttons after four seconds
 const closeBtns = Array.from(document.getElementsByClassName("btn-close"));
-console.log(closeBtns);
 
 closeBtns.forEach((i) => {
   setTimeout(() => i.click(), 4000);
